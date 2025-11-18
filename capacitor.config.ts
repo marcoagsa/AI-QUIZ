674b0168f4873d6d542d6c2e0ac65a8b35600292
Marco Sa <marco.sa@md3.pt>
2025-11-18 12:33:09 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'angular-quiz',
+  webDir: 'www'
+};
+
+export default config;
