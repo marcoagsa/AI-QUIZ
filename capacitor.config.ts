f1a312c8f3b16ca8f229ad62028fedb4f3d24e9e
Marco Sa <marco.sa@md3.pt>
2025-11-19 09:34:03 +0000

    update project name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'angular-quiz',
-  webDir: 'www'
+  appId: 'app.vercel.aiquiz',
+  appName: 'ai-quiz',
+  webDir: 'www',
 };
 
 export default config;
